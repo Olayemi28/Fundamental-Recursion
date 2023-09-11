92095a51d0db91d107b88c535beadb9ae56eefee
Olayemi28 <oyekunle.amotul.fareed2004@gmail.com>
2023-09-11 13:02:07 +0100

    third commit

---
+++ RandomRecursionQuestions/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+
+namespace Question_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 2, 3, 4 };
+            Recursive.PrintPermutation(array);
+
+            /*int n, i;
+            Recursive test = new Recursive();
+            int[] arr1 = new int[5];
+
+            Console.WriteLine("\n\n Recursion : Generate all possible permutations of an array :");
+            Console.WriteLine("------------------------------------------------------------------");
+
+            Console.Write(" Input the number of elements to store in the array [maximum 5 digits ] :");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.Write(" Input {0} number of elements in the array :\n", n);
+            for (i = 0; i < n; i++)
+            {
+                Console.Write(" element - {0} : ", i);
+                arr1[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.Write("\n The Permutations with a combination of {0} digits are : \n", n);
+            test.prnPermut(arr1, 0, n - 1);
+            Console.Write("\n\n");*/
+
+            //int[] array = { 1, 2, 3 };
+            //Recursive.PrintPermutation(array);
+            /*//SOLUTION TO QUESTION 1
+            Console.WriteLine("\t\tTHIS PROGRAM PRINTS NATURAL NUMBERS");
+            Recursive.NaturalNumber(15);
+            //================================================================================
+            //SOLUTION TO QUESTION 2
+            Console.WriteLine("\n\n");
+            Console.WriteLine("\t\tTHIS PROGRAM PRINTS NATURAL NUMBERS IN REVERSED ORDER");
+            Recursive.NaturalNumberInReversedOrder(15);
+            //==================================================================================
+            //SOLUTION TO QUESTION 3
+            Console.WriteLine("\n\n");
+            Console.WriteLine("\t\tTHIS PROGRAM PRINTS THE SUM OF NATURAL NUMBERS");
+            Console.WriteLine(Recursive.SumUpNaturalNumbers(10));
+            //==================================================================================
+            //SOLUTION TO QUESTION 4
+            Console.WriteLine("\n\n");
+            Console.WriteLine("\t\tTHIS PROGRAM PRINTS THE INDIVIDUAL DIGITS OF A NUMBER");
+            Recursive.IndividualDigits(12345);
+            //==================================================================================
+            //SOLUTION TO QUESTION 5
+            Console.WriteLine("\n\n");
+            Console.WriteLine("\t\tTHIS PROGRAM PRINTS THE COUNTS OF DIGITS");
+            Console.WriteLine(Recursive.CountOfDigits(1234567891));
+            //==================================================================================
+            //SOLUTION TO QUESTION 6
+            Console.WriteLine("\n\n");
+            Console.WriteLine("\t\tTHIS PROGRAM PRINTS EVEN AND ODD NUMBERS");
+            Recursive.PrintEvenOrOdd();
+            //==================================================================================
+            //SOLUTION TO QUESTION 7
+            Console.WriteLine("\n\n");
+            Console.WriteLine("\t\tTHIS PROGRAM PRINTS PRIME NUMBERS");
+            Recursive.PrintPrime(37);
+            //==================================================================================
+            //SOLUTION TO QUESTION 8
+            Console.WriteLine("\n\n");
+            Console.WriteLine("\t\tTHIS PROGRAM PRINTS PALINDROME");
+            Console.WriteLine(Recursive.PrintPalindrome("malam"));
+            //==================================================================================
+            //SOLUTION TO QUESTION 9
+            Console.WriteLine("\n\n");
+            Console.WriteLine("\t\tTHIS PROGRAM PRINTS THE FACTORIAL OF A NUMBER");
+            Console.WriteLine(Recursive.PrintFactorial(25));
+            //==================================================================================
+            //SOLUTION TO QUESTION 10
+            Console.WriteLine("\n\n");
+            Console.WriteLine("\t\tTHIS PROGRAM PRINTS THE FIBONACCI OF A GIVEN TERM");
+            Recursive.FindFibonacci(11);
+            //===================================================================================
+            //SOLUTION TO QUESTION 12
+            Console.WriteLine("\n\n");
+            Console.WriteLine("\t\tTHIS PROGRAM PRINTS THE LCM AND GCD OF A GIVEN NUMBER");
+            Recursive.PrintLCMandGCD(12, 6);
+            //===================================================================================
+            //SOLUTION TO QUESTION 13
+            Console.WriteLine("\n\n");
+            Console.WriteLine("\t\tTHIS PROGRAM CONVERT FROM DECIMAL NUMBER TO BINARY NUMBER");
+            Recursive.ConvertDecimalToBinary(158);
+            //=======================================================================================
+            //SOLUTION TO QUESTION 14
+            Console.WriteLine("\n\n");
+            Console.WriteLine("\t\tTHIS PROGRAM PRINTS THE REVERSE ORDER OF A GIVEN WORD OR SENTENCE");
+            Recursive.ReversedWord("AMOTUL-FAREED");
+            //==========================================================================================
+            //SOLUTION TO QUESTION 15
+            Console.WriteLine("\n\n");
+            Console.WriteLine("\t\tTHIS PROGRAM CALCULATE THE POWER OF A GIVEN NUMBER");
+            Console.WriteLine(Recursive.CalculatePower(3, 4));*/
+        }
+    }
+}
---
+++ RandomRecursionQuestions/Recursive.cs
@@ -0,0 +1,358 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_1
+{
+   public class Recursive
+   {
+        static int increment = 1;
+        static int sum;
+        static int count;
+
+        //1. Write a program in C# Sharp to print the first n natural number using recursion.
+       public static int NaturalNumber(int number)
+       {
+            if (number < 1)
+            {
+                return 1;
+            }
+            number--;
+            Console.Write(increment + " ");
+            increment++;
+            return NaturalNumber(number);
+       }
+
+        //2. Write a program in C# Sharp to print numbers from n to 1 using recursion.
+       public static int NaturalNumberInReversedOrder(int number)
+       {
+            if (number < 1)
+            {
+                return 1;
+            }
+            Console.Write(number + " ");
+            number--;
+            return NaturalNumberInReversedOrder(number);
+       }
+
+        //3. Write a program in C# Sharp to find the sum of first n natural numbers using recursion.
+       public static int SumUpNaturalNumbers(int number)
+       {
+            if (number < 1)
+            {
+                return sum;
+            }
+            number--;
+            sum += increment;
+            increment++;
+            return SumUpNaturalNumbers(number);
+
+       }
+
+        //4. Write a program in C# Sharp to display the individual digits of a given number using recursion.
+       public static void IndividualDigits(int number)
+       {
+            if (count > 0)
+            {
+                return;
+            }
+            IndividualDigits2(number);
+            IndividualDigits(number);
+       }
+       public static void IndividualDigits2(int number)
+       {
+            string value = number.ToString();
+            char[] split = value.ToCharArray();
+            foreach (var item in split)
+            {
+                Console.Write(item + " ");
+                count++;
+            }
+       }
+
+        //5. Write a program in C# Sharp to count the number of digits in a number using recursion.
+       public static int CountOfDigits(int number)
+       {
+            if (count > 0)
+            {
+                return count;
+            }
+            IndividualDigits2(number);
+            return CountOfDigits(number);
+       }
+
+        //6. Write a program in C to print even or odd numbers in a given range using recursion.
+       public static void PrintEvenOrOdd()
+       {
+            Console.Write("Even numbers: ");
+            PrintEvenNumber(6, 20);
+            Console.WriteLine();
+            Console.Write("Odd numbers: ");
+            PrintOddNumber(6, 20);
+       }
+       public static void PrintEvenNumber(int smaller, int bigger)
+       {
+            if (bigger < smaller)
+            {
+                return;
+            }
+            if (smaller % 2 == 0)
+            {
+                Console.Write(smaller + " ");
+            }
+            smaller++;
+            PrintEvenNumber(smaller, bigger);
+       }
+       public static void PrintOddNumber(int starting, int ending)
+       {
+            if (ending < starting)
+            {
+                return;
+            }
+            if (starting % 2 != 0)
+            {
+                Console.Write(starting + " ");
+            }
+            starting++;
+            PrintOddNumber(starting, ending);
+       }
+
+        //7. Write a program in C# to check whether a number is prime or not using recursion.
+        static int increment1 = 1;
+        static int count1;
+        public static void PrintPrime(int number)
+        { 
+            if(increment1 >= number)
+            {
+                if(count1 == 2)
+                {
+                    Console.WriteLine(number + " " + "is a prime number.");
+                    return;
+                }
+            }
+            if(number % increment1 == 0)
+            {
+                count1++;
+                if (count1 > 2)
+                {
+                    Console.WriteLine(number + " " + "is not a prime number.");
+                    return;
+                }
+            }
+            increment1++;
+            PrintPrime(number);
+        }
+
+        static int i;
+        static bool isPalindrome;
+        //8. Write a program in C# to check whether a string is palindrome or not using recursion.
+        public static bool PrintPalindrome(string word)
+        {
+            char[] letters = word.ToCharArray();
+            char[] reverse = word.Reverse().ToArray();
+            
+            if (letters.Length - 1 <= i)
+            {
+                if (letters[i] == reverse[i])
+                {
+                    return isPalindrome = true;
+                }
+            }
+            
+            if (letters[i] != reverse[i])
+            {
+               return isPalindrome;
+            }
+            i++;
+            return PrintPalindrome(word);
+        }
+
+        static BigInteger factorial = 1;
+        //9. Write a program in C# to find the factorial of a given number using recursion.
+        public static BigInteger PrintFactorial(int number)
+        {
+            if (number < 1)
+            {
+                return factorial;
+            }
+            
+           factorial *= number;
+           return PrintFactorial(number - 1);
+        }
+
+        static int a;
+        static int b = 1;
+        static int c;
+        static int counter = 1;
+        //10. Write a program in C# to find the fibonacci numbers for a N numbers using recursion.
+        public static void FindFibonacci(int number)
+        {
+            if (number == counter)
+            {
+                return;
+            }
+
+            if (number > 2)
+            {
+                if (counter == 1)
+                {
+                    Console.Write(0 + " ");
+                }
+                a = b;
+                b = c;
+                c = a + b;
+                counter++;
+            }
+            Console.Write(c + " ");
+            FindFibonacci(number);
+        }
+
+        static int k;
+        static int currentCount = 1;
+        //11. Write a program in C# to generate all possible permutation of an array using recursion.
+        public static void PrintPermutation(int[] array)
+        {
+            int i;
+            if (k == array.Length - 1)
+            {
+                for (i = 0; i <= array.Length - 1; i++)
+                {
+                    Console.Write($"{array[i]}");
+                }
+                Console.Write(" ");
+                k = 1;
+            }
+            else
+            {
+                for (i = k; i <= array.Length - 1; i++)
+                {
+                    SwapNumbers(ref array[k], ref array[i]);
+                    k++;
+                    PrintPermutation(array);
+                    SwapNumbers(ref array[k], ref array[i]);
+                }
+            }
+        }
+
+
+
+
+        public static void SwapNumbers(ref int num1, ref int num2)
+        {
+            int num3 = num1;
+            num1 = num2;
+            num2 = num3;
+        }
+
+
+        public void swapTwoNumber(ref int a, ref int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+        public void prnPermut(int[] list, int k, int m)
+        {
+            int i;
+            if (k == m)
+            {
+                for (i = 0; i <= m; i++)
+                    Console.Write("{0}", list[i]);
+                Console.Write(" ");
+            }
+            else
+                for (i = k; i <= m; i++)
+                {
+                    swapTwoNumber(ref list[k], ref list[i]);
+                    prnPermut(list, k + 1, m);
+                    swapTwoNumber(ref list[k], ref list[i]);
+                }
+        }
+
+
+        static int min;
+        static int j = 1;
+        static int max;
+        
+        //12. Write a program in C# to find the LCM and GCD of two numbers using recursion.
+        public static void PrintLCMandGCD(int num1, int num2)
+        {
+            if (num1 < num2)
+            {
+                min = num1;
+            }
+            else
+            {
+                min = num2;
+            }
+
+            if (num1 % j == 0 && num2 % j == 0)
+            {
+                if (j > max)
+                {
+                    max = j;
+                }
+            }
+
+            if (j == min)
+            {
+                Console.WriteLine($"LCM = {(Math.Abs(num1 * num2) / max)}, GCD = {max}");
+                return;
+            }
+            j++;
+            PrintLCMandGCD(num1, num2);
+        }
+
+        static List<long> save = new List<long>();
+        static long remainder;
+        
+        //13. Write a program in C# to convert from decimal to binary using recursion.
+        public static void ConvertDecimalToBinary(int number)
+        {
+            if (number < 1)
+            {
+                save.Reverse();
+                Console.Write(string.Join(" ", save));
+                return;
+            }
+
+            remainder = number % 2;
+            save.Add(remainder);
+            number = number / 2;
+            ConvertDecimalToBinary(number);
+        }
+
+        static int iteration;
+        static int counting;
+        //14. Write a program in C# to get reverse string using recursion.
+        public static void ReversedWord(string word)
+        {
+            char[] split = word.ToCharArray();
+            iteration = split.Length - 1;
+            if (counting == split.Length)
+            {
+                return;
+            }
+            iteration = iteration - counting;
+            Console.Write(split[iteration]);
+            counting++;
+            ReversedWord(word);
+        }
+
+        static int result = 1;
+        //15. Write a program in C# to calculate the power of any number using recursion.
+        public static int CalculatePower(int number, int power)
+        {
+            if (power < 1)
+            {
+                return result;
+            }
+            result *= number;
+            power--;
+            return CalculatePower(number, power);
+        }
+   }
+}
